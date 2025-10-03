f75537a888652e110ea6fdeb41237627268a9348
Murod <abdumajidov.murod@sarkor.uz>
2025-10-03 21:18:48 +0500

    not static nested class

--- src/oop/nested_classes/Human.java
+++ src/oop/nested_classes/Human.java
@@ -2,9 +2,11 @@ package oop.nested_classes;
 
 public class Human {
     private Passport passport;
+    private Heart heart;
 
-    public Human(String series, String number, PassportType passwordType) {
+    public Human(String series, String number, PassportType passwordType, double weight) {
         this.passport = new Passport(series, number, passwordType);
+        this.heart = new Heart(weight);
     }
 
     public static class Passport {
@@ -36,6 +38,21 @@ public class Human {
     public String toString() {
         return "Human{" +
                 "passport=" + passport +
+                ", heart=" + heart +
                 '}';
+    }
+
+    public class Heart {
+        private double weight;
+        public Heart(double weight) {
+            this.weight = weight;
+        }
+
+        @Override
+        public String toString() {
+            return "Heart{" +
+                    "weight=" + weight +
+                    '}';
+        }
     }
 }
--- src/oop/nested_classes/Runner.java
+++ src/oop/nested_classes/Runner.java
@@ -2,7 +2,7 @@ package oop.nested_classes;
 
 public class Runner {
     static void main() {
-        Human human = new Human("4323b54b", "435342rrr", PassportType.INNER);
+        Human human = new Human("4323b54b", "435342rrr", PassportType.INNER, 285.0);
         System.out.println(human);
     }
 }
