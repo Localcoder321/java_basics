dd450f5ddc77ae02170595b924b1655f779ad3fb
Murod <abdumajidov.murod@sarkor.uz>
2025-10-04 13:27:12 +0500

    fixed error

--- src/oop/polimorphism/difference_interface_and_abstract_class/Swan.java
+++ src/oop/polimorphism/difference_interface_and_abstract_class/Swan.java
@@ -1,6 +1,10 @@
 package oop.polimorphism.difference_interface_and_abstract_class;
 
 public class Swan extends Bird implements Swimable {
+    public Swan(String color) {
+        super(color);
+    }
+
     @Override
     public void swim() {
         System.out.println("Swan swim");
